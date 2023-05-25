78b5aa06b50523b25ec3c188665f6424f23e5286
Oswaldo <oswaldohuillca@gmail.com>
2023-05-25 17:15:16 -0500

    Add const wpCredentils object

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+declare namespace credentials {
+
+  interface Credentials {
+    domain: string
+    public_url: string
+    security: string
+    url: string
+  }
+
+  const credentials: Credentials
+}
+
+type Credentials = credentials.Credentials
+
+const wpCredentials: Credentials = credentials.credentials
